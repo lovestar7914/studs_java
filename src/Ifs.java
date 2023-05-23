d88b5f48a4af3184daafcfb302544b8af16d2f2a
lovestar7914 <jinaseong98.com>
2023-05-23 17:28:02 +0900

    commit

--- src/Ifs.java
+++ src/Ifs.java
@@ -1,5 +1,17 @@
 public class Ifs {
     public static void main(String[] args) throws Exception 
-        double myDouble  {
+       // if (condition) 
+      //     // block of code to be executed if the condition is true
+     //    } else {
+        intfirst =20;
+        intsecond =18;
+        if(float> second){fist = 20, second =18
+//     system.out.println(firsr);
+     //  }
+     System.out.println(x:20);
+     }
+     System.out.println(x: "Hell, World!");
+     // return 0;
+       {
     
 }
