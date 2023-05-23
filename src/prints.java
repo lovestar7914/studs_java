fdc8c4e2ea6e41cbb5fa944f0ac8ed97abe75811
lovestar7914 <jinaseong98.com>
2023-05-23 14:18:36 +0900

    first commit

---
+++ src/prints.java
@@ -0,0 +1,9 @@
+public class prints { 
+    public static void main(String[] args) throws Exception {
+        String name = "빅테이터 java 교육과정";
+        System.out.printl("빅테이터 java 교육과정");
+    }
+}
+
+    
+
