ec032c4d8e4ca998066a9a019a13018a954f51ba
lovestar7914 <jinaseong98.com>
2023-05-23 16:27:00 +0900

    Main

--- src/prints.java
+++ src/prints.java
@@ -2,7 +2,7 @@ public class prints {
     public static void main(String[] args) throws Exception {
         String name = "빅테이터 java 교육과정";
         System.out.printl ("빅테이터 java 교육과정");
-        return 0;
+        //return 0;
      
    
       }
