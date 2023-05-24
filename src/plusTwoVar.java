e57e3d52d4cedf52268a4421b4b32910854f1277
lovestar7914 <jinaseong98.com>
2023-05-24 14:18:10 +0900

    PlusTwoVar.java

---
+++ src/plusTwoVar.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class plusTwoVar {
+        public static void main(String[] args)  {
+            Scanner myObj = new Scanner(System.in);
+          
+        
+           int A = myObj.nextInt();
+           int B = myObj.nextInt();
+            System.out.println(A + B);
+   
+           //return 0;
+        }
+    }
+    
+
+    
