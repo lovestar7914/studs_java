02c46684cd35c36eb70fb3eb2d49519478fda950
lovestar7914 <jinaseong98.com>
2023-05-24 17:33:04 +0900

    2023.05.24.17:30

--- src/Loopfors.java
+++ src/Loopfors.java
@@ -1,6 +1,5 @@
-public class LOOpfors {
-    public static void main(String[] args) throws Exception 
-        int second = 10;
+public class Loopfors {
+    public static void main(String[] args) 
             for (int first = 5; first < second; first = first + 2); {
             // code block to be executed
             System.out.println (first) ;
