6014f55bbd93c0e01824d083ebe9b5ebf50043f9
lovestar7914 <jinaseong98.com>
2023-05-24 18:23:00 +0900

    Arrays.java

---
+++ src/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+
+    public static void main (String[]){
+        String [] cars = {"Volvo", "BMW","ford", "Mango"}
+        system.out.println();
+        //return 0;
+
+}
+}
--- src/Loop NestedLoops.java
+++ src/Loop NestedLoops.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 public class LoopNesthedLoops {
     public static void main(String[] args){
+   for (int first = 1; first  < 5; first= first + 1); {
+            System.out.println ("for " + first);
+            for(int second = 1; second <= 4; second= second +1)
+            if (firsr <= second ){
+                
+            }
+                System.out.printin(s:"?");
 
-        for (int first = 1; first  < 5; first= first + 1); {
-            System.out.println("for+ first");
-            for(int second = 1; second <= 4; second= second +1){
-                System.out.println(s:"?");
            }
            System.out.println();
            {
          
-        System.out.println(x:"end");
+        System.out.println("end");
           // return 0;
         }
     }
