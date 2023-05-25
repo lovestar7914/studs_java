6050ef89e5531095293698d4d136ae39f8dd193b
lovestar7914 <jinaseong98.com>
2023-05-25 16:29:55 +0900

    myobj,java

---
+++ src/PollesWihtoutMetod.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class pollesWithoutMetod {
+    public static void main(String[] args)  
+    Scanner myObj = new Scanner(System.in);
+    String = answer =","
+    String[] answer ={"",""};
+
+    System.out.println("1.문항" );
+
+    System.out.println(null(1) 답항"+""(2) 답항"+"(3)답항");
+    System.out.print(s:"답하기:")
+    answer[0]= myobj.nextLine();
+}
+System.out.println
+System.out.println
+answer[1]=myobj.nextLine();
+for int first =0; first < answers. length; first =first +1{
+    System.out.println(acswers[first]+",");
+}
+System.out.println();
+//return 0;
