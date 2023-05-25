b0d1f4c22accf96248787166b345f91fae0f3363
lovestar7914 <jinaseong98.com>
2023-05-25 15:52:24 +0900

    txte ifs.java

---
+++ src/txte ifs.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class txte ifs { 
+   public static void main (String[]args)throws Exception{
+        // if (condition) 
+      //     // block of code to be executed if the condition is true
+     //    } else }
+
+        int a = 2 ;
+        int b = 4 ;
+        int c = 6 ;
+        int d = 8 ;
+        System.out.println(a) ;
+        System.out.println(b) ;
+        System.out.println(c) ;
+        System.out.println(d) ;
+   }   
+}
+
+
