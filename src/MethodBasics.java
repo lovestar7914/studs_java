22bab2872155c63e7291a48bcd220c948a7d37a6
lovestar7914 <jinaseong98.com>
2023-05-30 13:17:31 +0900

    studs_java

--- src/MethodBasics.java
+++ src/MethodBasics.java
@@ -1,27 +1,29 @@
-public class MethodBasics{
-    //<access_modifier><return_type> <method_name>()
-    //{
-    // //body    
-    //}
+public class MethodBasics {
+    // <access_modifier><return_type> <method_name>()
+    // {
+    // //body
+    // }
     // x에 +1
-    public int F(int x)
-    {
-     int result = x + 1;
-     return result;
+    public int F(int x) {
+        int result = x + 1;
+        return result;
 
     }
-    
+
     public static void main(String[] args){
-   try {
-    int x = 4;
-    int y = 0; 
-    //y = x + 1; //변수에 +1
-    y = x +  1; 
-    y = F(X);
-    y = y +  1; // 변수에 +1
-   }catch(Exception e){
+       try {
+        int x = 4;
+        int y = 0; 
+        //y = x + 1; //변수에 +1
+        //y = f(x);
+        MethodBasics methodbasics = new MethodBasics();
+        y = methodbasics.F(x);
+       
+        y = y +  1; // 변수에 +1
+    }catch(Exception e){
     //TODO: handle exception
+    }
+     System.out.println();
+      //return 0;
    }
-   System.out.println();
-   //return 0;
-}
+}   
