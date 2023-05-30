0cc5ae1b4cbb620430b855ba264210b207aedf24
lovestar7914 <jinaseong98.com>
2023-05-30 15:27:52 +0900

    studs_java

--- src/MethodBasics.java
+++ src/MethodBasics.java
@@ -4,26 +4,35 @@ public class MethodBasics {
     // //body
     // }
     // x에 +1
-    public int F(int x) {
+    public int F(int x) // x = 0
+    {
         int result = x + 1;
         return result;
-
     }
 
-    public static void main(String[] args){
-       try {
-        int x = 4;
-        int y = 0; 
-        //y = x + 1; //변수에 +1
-        //y = f(x);
-        MethodBasics methodbasics = new MethodBasics();
-        y = methodbasics.F(x);
-       
-        y = y +  1; // 변수에 +1
-    }catch(Exception e){
-    //TODO: handle exception
+    public static void main(String[] args) {
+        try {
+            int x = 4;
+            int y = 0;
+            // y = x + 1; //변수에 +1
+            // y = f(x);
+            MethodBasics methodbasics = new MethodBasics();
+            y = methodbasics.F(x);
+
+            y = y + 1; // 변수에 +1
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        System.out.println();
+        // return 0;
     }
-     System.out.println();
-      //return 0;
-   }
-}   
+}
+  // y = y + 1; //변수에 +1
+    MethodBasics methodbasics_first = new MethodBasics();
+    y = methodbasics_first.F(0); //F(0)
+    y = methodbasics_first.F(y); //F(0)
+
+    
+
+
+   
