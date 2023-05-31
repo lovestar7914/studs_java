4ee5ac059d583941abd997db7a896f2aa787e23c
AKAVespaET3 <quattrini244@gmail.com>
2023-05-31 12:30:14 +0900

    Goni

---
+++ src/Goni.java
@@ -0,0 +1,10 @@
+public class Goni {
+    public static void main(String[] args) {
+        try {
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        } 
+        // return 0 ;
+    }
+}
