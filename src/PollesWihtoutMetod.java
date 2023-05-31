e9ea0155570d889d2695952d539fa7386033869a
lovestar7914 <jinaseong98.com>
2023-05-31 18:25:22 +0900

    studs_java

--- src/PollesWihtoutMetod.java
+++ src/PollesWihtoutMetod.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 public class PollesWihtoutMetod {
   public static void main(String[] args){ 
 
-    Scanner myObj = new Scanner(System.in);
-    String = answer ="";
+    Scanner myObj = new Scanner(System.in);{
+      System.out.println(null);// 이름 입력 하세요
+       try{
+    String  = answer = "";
     String[] answer ={"",""};
     String[][]polls= {
          {"1.문항" },
-         {"(1)답항", "(2)"답항", "(3)답항"},
+         {"(1)답항", "(2)"답항" , "(3) 답항"},
          {"2.문항},
          {"(1)답항", "(2)"답항", "(3)답항"};
       int count =0;
